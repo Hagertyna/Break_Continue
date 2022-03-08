df50d3075b10d18ef9cb9ec4fc4987a605da6efe
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-03-08 05:53:09 -0800

    Add files via upload
    Break and Continue

---
+++ Break.py
@@ -0,0 +1,12 @@
+import random as r
+randnum = r.randrange(1,30)
+print("Number to be gussed...",randnum)
+i =1
+while True:
+    print("number guessed :", i)
+    if(i == randnum):
+        print("Rando number has been guessed correctly Congrates")
+        #break and exits from the loop
+        break
+    i +=1
+print("the program is completed.")
---
+++ continuwe.py
@@ -0,0 +1,5 @@
+# prints even number between 1 and 29 since upper limit is excluded
+for i in range(1,30):
+    if(i%2 != 0):
+        continue # continue current iteration
+    print(i)
