c9354e63c4eafcf037f9b473696cfc2f203ed025
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-07-13 00:22:27 -0700

    Create While_Continue.py

---
+++ While_Continue.py
@@ -0,0 +1,33 @@
+# Python program to
+# demonstrate break statement
+ 
+s = 'geeksforgeeks'
+# Using for loop
+for letter in s:
+ 
+    print(letter)
+    # break the loop as soon it sees 'e'
+    # or 's'
+    if letter == 'e' or letter == 's':
+        break
+ 
+print(& quot
+       Out of for loop & quot
+       )
+print()
+ 
+i = 0
+ 
+# Using while loop
+while True:
+    print(s[i])
+ 
+    # break the loop as soon it sees 'e'
+    # or 's'
+    if s[i] == 'e' or s[i] == 's':
+        break
+    i += 1
+ 
+print(& quot
+       Out of while loop & quot
+       )
