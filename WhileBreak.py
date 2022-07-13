6ee2982f2d8b7358b3b58973368807e77231c009
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-07-12 22:56:54 -0700

    Create WhileBreak.py

---
+++ WhileBreak.py
@@ -0,0 +1,8 @@
+var = 10 # Second instance
+while var > 0. print 'Current variable value :', var              
+   print('Current variable value :', var)
+   var = var -1
+   if var == 5: # Exit the loop when the variable var is equal to 5
+      break
+
+print "Good bye!"
